9fc1d765a07dacc1747a3a30ab196bc0c89b82c1
shota okunaka <okkun@shotanoMacBook-puro.local>
2018-07-20 00:25:29 +0900

    added fizzbuzz

---
+++ fizzbuzz.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	for i := 1; i < 100; i++ {
+		fmt.Println(i)
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("fizzbuzz")
+		} else if i%3 == 0 {
+			fmt.Println("fizz")
+		} else if i%5 == 0 {
+			fmt.Println("buzz")
+		}
+	}
+}
